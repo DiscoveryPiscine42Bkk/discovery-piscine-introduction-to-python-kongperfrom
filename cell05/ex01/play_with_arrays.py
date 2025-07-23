cb4706c41296f9178f780b82d3bf093acc673d96
kongperfrom <68040045@kmitl.ac.th>
2025-07-23 11:33:20 +0000

    dsfasfasfdas

--- cell05/ex01/play_with_arrays.py
+++ cell05/ex01/play_with_arrays.py
@@ -1,2 +1,4 @@
-Original array: [2, 8, 9, 48, 8, 22, -12, 2]
-New array: [4, 10, 11, 50, 10, 24, -10, 4]
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+new_array = [num + 2 for num in original_array]
+print("Original array:", original_array)
+print("New array:", new_array)
